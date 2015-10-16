9b7a86e4d20858b9f7ad37abff1cfcce41087391
chetan bhalerao <chetan@leftshift.io>
2015-10-16 15:48:34 +0530

    created user detail fragment

--- app/src/main/java/id/co/veritrans/sdk/example/MainActivity.java
+++ app/src/main/java/id/co/veritrans/sdk/example/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import id.co.veritrans.sdk.UserDetailsActivity;
+import id.co.veritrans.sdk.activities.UserDetailsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
