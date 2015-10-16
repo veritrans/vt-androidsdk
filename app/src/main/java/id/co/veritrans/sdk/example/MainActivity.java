7880085257aabbeb11e0d3b01e8627942c0fd3e8
shivam340 <shivam@leftshift.io>
2015-10-16 15:29:53 +0530

    removed unwanted components

--- app/src/main/java/id/co/veritrans/sdk/example/MainActivity.java
+++ app/src/main/java/id/co/veritrans/sdk/example/MainActivity.java
@@ -1,9 +1,12 @@
 package id.co.veritrans.sdk.example;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import id.co.veritrans.sdk.UserDetailsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,27 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button payment = (Button) findViewById(R.id.btn_payment);
+
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, UserDetailsActivity.class));
+
+            }
+        });
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
