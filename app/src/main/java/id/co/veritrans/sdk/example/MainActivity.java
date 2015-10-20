f4f01e71cb9f194e22765825ad4f6bb2e4c98186
shivam340 <shivam@leftshift.io>
2015-10-20 19:23:52 +0530

    tried to create two instances of sdk

--- app/src/main/java/id/co/veritrans/sdk/example/MainActivity.java
+++ app/src/main/java/id/co/veritrans/sdk/example/MainActivity.java
@@ -2,12 +2,17 @@ package id.co.veritrans.sdk.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import id.co.veritrans.sdk.core.VeritransBuilder;
+import id.co.veritrans.sdk.core.VeritransSDK;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,23 @@ public class MainActivity extends AppCompatActivity {
 
                 VeritransBuilder veritransBuilder = new
                         VeritransBuilder(MainActivity.this, "dbdy", 100, true);
+                veritransBuilder.enableLog(true);
                     veritransBuilder.buildSDK();
+
+
+
+                VeritransBuilder veritransBuilder2 = new
+                        VeritransBuilder(MainActivity.this, "dbdy", 100, true);
+
+
+                VeritransSDK veritransSDK2 = veritransBuilder2.buildSDK();
+
+                if(veritransSDK2 == null){
+                    Log.d(TAG , "failed to create sdk instance.");
+                }else{
+                    Log.d(TAG , "successfully created sdk instance.");
+                }
+
             }
         });
 
