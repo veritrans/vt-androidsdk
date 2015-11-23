e4dfff6aef3331055700a02f88feec76b21650eb
shivam340 <shivam@leftshift.io>
2015-11-23 19:48:41 +0530

    added merchant urls

--- app/src/main/java/id/co/veritrans/sdk/example/utils/Constants.java
+++ app/src/main/java/id/co/veritrans/sdk/example/utils/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String VT_CLIENT_KEY = "VT-client-Lre_JFh5klhfGefF";
     public static final String VT_SERVER_KEY = "VT-server-pVDm2d9BT3TIbDls_UEmt8wE";
 
+    public static final String BASE_URL_MERCHANT_FOR_DEBUG = "https://hangout.betas.in/veritrans/api/";
+    public static final String BASE_URL_MERCHANT_FOR_RELEASE = "https://hangout.betas.in/veritrans/api/";
 }
