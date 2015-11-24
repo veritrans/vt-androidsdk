5775097d92ce2542bc3b79f18b94b09e465e55b2
shivam340 <shivam@leftshift.io>
2015-11-24 15:34:18 +0530

    removed unwanted code and added new files

---
+++ app/src/main/java/id/co/veritrans/sdk/example/model/UpdateTransactionCallBack.java
@@ -0,0 +1,13 @@
+package id.co.veritrans.sdk.example.model;
+
+import id.co.veritrans.sdk.models.TransactionUpdateMerchantResponse;
+
+/**
+ * Created by shivam on 10/29/15.
+ */
+public interface UpdateTransactionCallBack {
+    public void onSuccess(TransactionUpdateMerchantResponse transactionUpdateMerchantResponse);
+
+    public void onFailure(String errorMessage, TransactionUpdateMerchantResponse
+            transactionUpdateMerchantResponse);
+}
