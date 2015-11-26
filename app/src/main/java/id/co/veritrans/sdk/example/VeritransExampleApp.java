8e4c20de0b179877bed13c8b432f8e92682e3462
shivam340 <shivam@leftshift.io>
2015-11-26 20:02:51 +0530

    #VER-84 #time 8h 40m added documentation, fix code and added separate code flow for ui and core

--- app/src/main/java/id/co/veritrans/sdk/example/VeritransExampleApp.java
+++ app/src/main/java/id/co/veritrans/sdk/example/VeritransExampleApp.java
@@ -4,17 +4,43 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 
+import id.co.veritrans.sdk.core.VeritransBuilder;
+import id.co.veritrans.sdk.core.VeritransSDK;
+import id.co.veritrans.sdk.example.utils.Constants;
 import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by chetan on 17/11/15.
  */
 public class VeritransExampleApp extends Application {
+
+    private VeritransSDK mVeritransSDK = null;
+
     @Override
     public void onCreate() {
         super.onCreate();
         if (!BuildConfig.DEBUG) {
             Fabric.with(this, new Crashlytics());
         }
+
+        initializeSdk();
+    }
+
+
+    /**
+     * initialize veritrans sdk at the beginning of the application.
+     */
+    private void initializeSdk() {
+        // sdk initialization process
+        VeritransBuilder veritransBuilder = new
+                VeritransBuilder(getApplicationContext(),
+                Constants.VT_CLIENT_KEY, Constants.VT_SERVER_KEY);
+        veritransBuilder.enableLog(true);
+        mVeritransSDK = veritransBuilder.buildSDK();
+    }
+
+
+    public VeritransSDK getVeritransSDK() {
+        return mVeritransSDK;
     }
 }
