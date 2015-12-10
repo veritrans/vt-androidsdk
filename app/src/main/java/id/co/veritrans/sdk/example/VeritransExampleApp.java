be89df8980228e231f6e83d01f0e987911699d97
chetan bhalerao <chetan@leftshift.io>
2015-12-10 15:46:16 +0530

    VER-136 #time 2h 20m added documentation about push notification, added condition to registration token api call

--- app/src/main/java/id/co/veritrans/sdk/example/VeritransExampleApp.java
+++ app/src/main/java/id/co/veritrans/sdk/example/VeritransExampleApp.java
@@ -20,38 +20,14 @@ public class VeritransExampleApp extends Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
-        /*if (!BuildConfig.DEBUG) {
+        if (!BuildConfig.DEBUG) {
             Fabric.with(this, new Crashlytics());
-        }*/
+        }
         initializeSdk();
        // broadcastPushTest();
     }
 
-  /*  public void broadcastPushTest() {
-        Intent intent = new Intent("com.google.android.gms.gcm.GcmReceiver");
-        String jsonString = "{\n" +
-                " \"status_code\" : \"201\",\n" +
-                " \"status_message\" : \"Success, CIMB Clicks transaction is successful\",\n" +
-                " \"transaction_id\" : \"d5f2d082-ce28-4fd1-a999-9a85bc566a9e\",\n" +
-                " \"order_id\" : \"a8572607d8\",\n" +
-                " \"gross_amount\" : \"100.00\",\n" +
-                " \"payment_type\" : \"cimb_clicks\",\n" +
-                " \"transaction_time\" : \"2015-12-01 14:40:01\",\n" +
-                " \"transaction_status\" : \"pending\"\n" +
-                "}";
-        intent.putExtra("message",jsonString);
-        Logger.i("broadcast:"+jsonString);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        long now = System.currentTimeMillis();
-        long interval = 10 * 1000;
-        manager.setRepeating(AlarmManager.RTC_WAKEUP, now + interval, interval,
-                pendingIntent);
-    }*/
 
-    /**
-     * initialize veritrans sdk at the beginning of the application.
-     */
     private void initializeSdk() {
         // sdk initialization process
         VeritransBuilder veritransBuilder = new
