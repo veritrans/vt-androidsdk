e50a1014c1d572726c91b04fd8b72beba97afadb
chetan bhalerao <chetan@leftshift.io>
2015-12-31 15:36:33 +0530

    #VER-162 #time 5h created functionality to show notification and created build

--- app/src/main/java/id/co/veritrans/sdk/example/gcmutils/VeritransGcmListenerService.java
+++ app/src/main/java/id/co/veritrans/sdk/example/gcmutils/VeritransGcmListenerService.java
@@ -16,22 +16,12 @@
 
 package id.co.veritrans.sdk.example.gcmutils;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
-import id.co.veritrans.sdk.activities.NotificationActivity;
 import id.co.veritrans.sdk.core.Logger;
-import id.co.veritrans.sdk.example.R;
-import id.co.veritrans.sdk.example.utils.Constants;
-import id.co.veritrans.sdk.models.TransactionResponse;
+import id.co.veritrans.sdk.utilities.NotificationUtils;
 
 public class VeritransGcmListenerService extends GcmListenerService {
 
@@ -71,46 +61,10 @@ public class VeritransGcmListenerService extends GcmListenerService {
          * In some cases it may be useful to show a notification indicating to the user
          * that a message was received.
          */
-        sendNotification(data);
+        NotificationUtils.sendNotification(getApplicationContext(),data);
         // [END_EXCLUDE]
     }
     // [END receive_message]
 
-    /**
-     * Create and show a simple notification containing the received GCM message.
-     *
-     * @param data GCM message received.
-     */
-    private void sendNotification(Bundle data) {
-        String statusCode = data.getString(Constants.KEY_STATUS_CODE,"");
-        String statusMessage = data.getString(Constants.KEY_STATUS_MESSAGE,"");
-        String transactionId = data.getString(Constants.KEY_TRANSACTION_ID,"");
-        String orderId = data.getString(Constants.KEY_ORDER_ID,"");
-        String grossAmount = data.getString(Constants.KEY_GROSS_AMOUNT,"");
-        String paymentType = data.getString(Constants.KEY_PAYMENT_TYPE,"");
-        String transactionTime = data.getString(Constants.KEY_TRANSACTION_TIME,"");
-        String transactionStatus = data.getString(Constants.KEY_TRANSACTION_STATUS,"");
-        TransactionResponse transactionResponse = new TransactionResponse( statusCode,  statusMessage,
-                transactionId, orderId,  grossAmount,  paymentType,transactionTime,  transactionStatus);
-        Intent intent = new Intent(this, NotificationActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra(Constants.PAYMENT_STATUS,transactionResponse);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.common_ic_googleplayservices)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(statusMessage)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        int randomPIN = (int)(Math.random()*9000)+1000;
-        notificationManager.notify(randomPIN, notificationBuilder.build());
-    }
 }
